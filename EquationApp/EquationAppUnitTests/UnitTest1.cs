03dd06eb6accbbefdbfadbb9a2d935ef0923c999
Sam Rule <s80cmdude@gmail.com>
2016-12-16 15:25:06 +0000

    Added the basis for Unit testing

---
+++ EquationApp/EquationAppUnitTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EquationApp;
+using NUnit.Framework;
+
+namespace EquationAppUnitTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            MainPage main = new MainPage();
+        }
+    }
+}
